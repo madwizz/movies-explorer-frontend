21ebba1f78304373fcd289a9835ab0f619d64e00
Dmitrii Sapegin <lejeboka24@gmail.com>
2023-03-06 13:33:42 +0400

    promo border-radius fix

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,7 +6,7 @@ function Promo() {
   return (
     <section className='promo'>
       <h1 className='promo__title'>
-        Учебный проект студента <br/> факультета Веб-разработки.
+        Учебный проект студента факультета Веб-разработки.
       </h1>
     </section>
   )
